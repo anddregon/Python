64290b1bb2a65f7facd453d9257c119f65c8192a
Felipe Gonzalez <xfelipe05@gmail.com>
2022-03-17 00:35:36 -0500

    Some stuff

--- Universidad_Python_Con_Frameworks_Django_Flask_Y_Mucho_Mas/21.Operadores_de_comparacion.py
+++ Universidad_Python_Con_Frameworks_Django_Flask_Y_Mucho_Mas/21.Operadores_de_comparacion.py
@@ -1,5 +1,25 @@
-a = int(input("Ingrese un numero"))
-b = int(input("Ingrese otro numero"))
+a = int(input("Ingrese un numero: "))
+b = int(input("Ingrese otro numero: "))
 
 #recordermos que el = es el operador de asignación
-resultado = (a == b)
+resultado = (a == b) #si a y b son iguales asignele el valor True a variable resultado
+print (f'¿{a} es igual a {b}? {resultado}')
+
+resultado = (a != b) #si a y b son diferentes asignele el valor True a variable resultado
+print (f'¿{a} es diferente que {b}? {resultado}')
+
+resultado = (a > b) #si a es mayor que b asignele el valor True a variable resultado
+print (f'¿{a} es mayor que {b}? {resultado}')
+
+resultado = (a < b) #si a es menor que b asignele el valor True a variable resultado
+print (f'¿{a} es menor que {b}? {resultado}')
+
+
+resultado = (a >= b) #si a es mayor o igual que b asignele el valor True a variable resultado
+print (f'¿{a} es mayor o igual que {b}? {resultado}')
+
+resultado = (a <= b) #si a es menor o igual que b asignele el valor True a variable resultado
+print (f'¿{a} es menor o igual que {b}? {resultado}')
+
+
+
