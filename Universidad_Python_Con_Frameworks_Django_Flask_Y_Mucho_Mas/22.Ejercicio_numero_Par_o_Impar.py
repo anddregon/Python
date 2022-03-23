3c05ea5b8ec7bd215e914876acdf01faf7de5260
Felipe Gonzalez <xfelipe05@gmail.com>
2022-03-22 19:29:39 -0500

    Curso Intermedio de Python, entorno virtual y demás

---
+++ Universidad_Python_Con_Frameworks_Django_Flask_Y_Mucho_Mas/22.Ejercicio_numero_Par_o_Impar.py
@@ -0,0 +1,7 @@
+a = int(input('Escribe un valor númerico: '))
+#== es el operador de comparación
+if a % 2 == 0:
+    print(f'El valor de a {a} es par')
+else:
+    print(f'El valor de a {a} es impar')
+
---
+++ Universidad_Python_Con_Frameworks_Django_Flask_Y_Mucho_Mas/23.Ejercicio_si_esmayor_de_edad.py
@@ -0,0 +1,13 @@
+edad_adulto = 18
+edad_persona = int(input('Proporciona tu edad: '))
+
+
+if edad_persona >= edad_adulto:
+    print (f'La personas con edad {edad_persona} es mayor de edad')
+
+else:
+    print (f'La personas con edad {edad_persona} es menor de edad')
+
+
+
+
