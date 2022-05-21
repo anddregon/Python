e7a4971995c68d91b77eb980c8694f9b03b872d6
Felipe Gonzalez <xfelipe05@gmail.com>
2022-05-21 01:45:34 -0500

    First example of Lambdas in Python

---
+++ Curso_de_Python_Intermedio_Comprenhensions_Lambdas_Manejos_de_Errores/curso-python/lambda_function_palindrome.py
@@ -0,0 +1,11 @@
+def main ():
+
+    # def palindrome(string):
+    #     return string == string[::-1]
+
+    # print(palindrome('ana'))    
+
+    palindrome = lambda string: string == string[::-1]
+    
+if __name__ == "__main__":
+    main()
