9588baf1101a782c951a7977746ec779efe9cd53
Felipe Gonzalez <xfelipe05@gmail.com>
2022-06-08 15:47:19 -0500

    Modo debug en Visual Studio Code

---
+++ Universidad_Python_Con_Frameworks_Django_Flask_Y_Mucho_Mas/42.Ejecucion_Modo_Debug.py
@@ -0,0 +1,35 @@
+def main():
+#Si en la condicion el valor es distinto de vacio guarda un True, por
+#ejemplo si introducimos un numero.
+
+#Si en la condicion el valor es una cadena vacia, ahí si se guarda 
+#un False
+
+#Eso anterior es en caso de que solo pongamos if condicion: sin el == True
+    
+#* Video: https://www.youtube.com/watch?v=xAG1FBEIf3g&t=108s
+#* Debug es un proceso informatico que se encarga de eliminar 
+#* errores, para ello en VScode tenemos diferentes herramientas.
+
+#* Para ello le damos en Run and debug en la mariquita del lado
+#* izquierdo/Run and Debug/marcamos un BreakPoint, y ya podemos jugar
+#* con el Step over, Step Into y demás... y si le damos click derecho
+#* en el break Point/Edit Break Point podemos crear Expressions, Hi
+#* Count y Log Messages
+
+    
+    condicion = input('Digite True o False: ')
+    
+
+    if condicion == 'True' or condicion == 'true':
+        print('Condicion verdadera')
+                   
+    elif condicion == 'False' or condicion == 'false':
+        print ('Condición Falsa')
+
+    else:
+        print('Condicion no reconocida')
+
+
+if __name__ == '__main__':
+    main()
