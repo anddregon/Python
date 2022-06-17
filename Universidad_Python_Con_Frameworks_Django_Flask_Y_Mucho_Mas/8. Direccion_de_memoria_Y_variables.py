11a265883945641f2fad93b4969bab08b220661a
Felipe Gonzalez <xfelipe05@gmail.com>
2022-06-16 22:52:53 -0500

    comentarios direccion de memoria

--- Universidad_Python_Con_Frameworks_Django_Flask_Y_Mucho_Mas/8. Direccion_de_memoria_Y_variables.py
+++ Universidad_Python_Con_Frameworks_Django_Flask_Y_Mucho_Mas/8. Direccion_de_memoria_Y_variables.py
@@ -4,6 +4,19 @@ def main():
     z = x + y
     #* I use f-strings to format strings, this is a new feature
     #* available since Python 3.6
+
+    #*Una literal es un valor que le asignamos a una variable
+    #*Las literales se almacenan en una direccion en memoria RAM
+    #*Literal 10 se almacena en una direccion en memoria 
+    #*Literal 2 se almacena en otra direccion en memoria
+    #*Las variables son las encargadas de apuntar a esa direccion en memoria
+    #*Con la función ID podemos obtener la direccion en memoria a la cual
+    #*apuntan las variables o en la que están ubicadas las literales
+    #*ID es una built in function https://docs.python.org/3/library/functions.html
+    #* Direcciones en memoria son usualmente en hexadecimal
+    #* Es normal que al volver a ejectuar el programa, cambien las direcciones en
+    #* memoria RAM pues recordemos que la memoria RAm es volatil
+
     print(f'this is the literal or the value of the variable: {x}')
     print(f'this is the literal or the value of the variable: {y}')
     print(f'this is the literal or the value of the variable: {z}')
